037d0362b91469e5c7722e4a91dec79054067203
Darwish98 <66258375+Darwish98@users.noreply.github.com>
2022-12-18 23:30:31 +0100

    Add project files.

---
+++ CityInfo.API/DbContexts/CityInfoContext.cs
@@ -0,0 +1,24 @@
+﻿using CityInfo.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CityInfo.API.DbContexts
+{
+    public class CityInfoContext : DbContext
+    {
+        public DbSet<City> Cities { get; set; } = null!; //about this -> 6.4, min 2:50 in video
+
+        public DbSet<PointOfInterest> PointsOfInterest { get; set; } = null!;
+
+
+        public CityInfoContext(DbContextOptions<CityInfoContext> options) : base(options)
+        {
+
+        }
+
+        /*override protected void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("connectionstring");
+            base.OnConfiguring(optionsBuilder);
+        }*/
+    }
+}
